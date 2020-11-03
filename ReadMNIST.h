3586fe6257cff88380a70dd5ea5afafbbdb41efb
GeorgiosKolias <73882230+GeorgiosKolias@users.noreply.github.com>
2020-11-03 12:56:10 +0200

    MNIST header

---
+++ ReadMNIST.h
@@ -0,0 +1,13 @@
+#ifndef READMNIST_H
+#define READMNIST_H
+
+#include <vector>
+
+#include "header.h"
+
+using namespace std;
+
+int ReadMNIST(char * input_file, vector<vector<X_TYPE>> &arr) ;
+
+#endif /* READMNIST_H */
+
